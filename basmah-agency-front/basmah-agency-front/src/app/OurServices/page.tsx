886d312172e6d0bcd823daf0fafd1cecb96c2449
Mohamed Gharieb <m.gharieb89@gmail.com>
2025-08-22 00:25:56 +0300

    header v1

---
+++ basmah-agency-front/basmah-agency-front/src/app/OurServices/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// This is the main page for basmah-agency app
+
+const OurServices = () => {
+  return <div className="text-5xl text-pink-500">Our Services</div>;
+};
+
+export default OurServices;
